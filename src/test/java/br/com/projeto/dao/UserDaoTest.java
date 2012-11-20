428f6fbb2b9304509d771a3f73065ff27e345ade
Marcos Paulo <slipmp@hotmail.com>
2012-11-20 14:59:42 -0200

    Tentando fazer rodar o JUNIT com o JPA

--- src/test/java/br/com/projeto/dao/UserDaoTest.java
+++ src/test/java/br/com/projeto/dao/UserDaoTest.java
@@ -16,7 +16,13 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import br.com.projeto.dao.UserDao;
 import br.com.projeto.entity.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -49,30 +55,52 @@ public class UserDaoTest {
 	public void tearDown() throws Exception {
 		System.out.println("tearDown");
 	}
-
 	@Test
-	public void testInsert() throws SQLException {
-		/*
+	public void testIniciandoOsTestes()
+	{
+		System.out.println("Iniciando os testes da UserDaoTest!");
+	}
+	@Test
+	public void testCreate() throws SQLException {
+		System.out.println("testCreate Iniciando!");
+		UserDao dao =new UserDao();
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("projeto");
+	    EntityManager em = emf.createEntityManager();
+	    dao.setEntityManager(em);
+	    
 		User u = new User();
 		u.setLogin("juca");
 		u.setPassword("123");
 		dao.insert(u);
-		Connection con = ds.getConnection();
-		Statement s = con.createStatement();
-		ResultSet rs = s.executeQuery(" select * from usuario");
-		while(rs.next()) {
-			System.out.println(rs.getString(0)+"-"+ rs.getString(1));
-		}*/
-		dao.findById(User.class, 1l);
-		System.out.println("testFindById");
-
-		System.out.println("testInsert");
-		//fail("Not yet implemented");
+		
+	    System.out.println("testCreate Concluido!");
 	}
 
 	@Test
-	public void testUpdate() {
-		System.out.println("testUpdate");
+	public void testRetrieve() throws SQLException {
+		Connection con = ds.getConnection();
+		Statement s = con.createStatement();
+		ResultSet rs = s.executeQuery(" select * from tbUsuario");
+		while(rs.next()) {
+			System.out.println(rs.getString("id")+"-"+ rs.getString("login"));
+		}
+		System.out.println("testRetrieve Concluido!");
+	}
+	
+	@Test
+	public void testUpdate() throws SQLException {
+		User u = dao.getUser("juca", "123");
+		u.setLogin("JucaUpdated");
+		dao.update(u);
+		System.out.println("testUpdate Concluido!");
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDelete() throws SQLException {
+		User u = dao.getUser("JucaUpdated", "123");
+		dao.remove(User.class,u.getId());
+		System.out.println("testDelete Concluido!");
 		//fail("Not yet implemented");
 	}
 }
